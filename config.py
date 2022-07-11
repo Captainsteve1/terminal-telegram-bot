c45a33766f94a726298dab1cfa795188fd8a27bb
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-07-11 12:57:01 +0530

    Update and rename termbot/sample_config.py to config.py

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ import os
 class Config(object):
     LOGGER = True
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", None)
+    APP_ID = int(os.environ.get("15855531")
+    API_HASH = os.environ.get("31e0b87de4285ebff259e003f58bf469", None)
     # Get these values from my.telegram.org
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", None)
+    TG_BOT_TOKEN = os.environ.get("5431085411:AAEg-tf2DgVzN2BLeIQdrhj7d0XJ8SX-DDA", None)
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # This is required for the plugins involving the file system.
