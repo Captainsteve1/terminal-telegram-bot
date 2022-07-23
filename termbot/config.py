abfbb787b13f2461a8b3c96c9f2be10f628e412f
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-07-23 17:12:04 +0530

    Update and rename sample_config.py to config.py

---
+++ termbot/config.py
@@ -0,0 +1,9 @@
+from termbot.sample_config import Config
+
+class Development(Config):
+  APP_ID = 15855531
+  API_HASH = "31e0b87de4285ebff259e003f58bf469"
+  TG_BOT_TOKEN = "5346883687:AAEqC3_XJoqEZ8iLK6e_3bYx5E0G3jkQnWQ"
+  AUTH_USERS = [
+    7351948
+  ]
